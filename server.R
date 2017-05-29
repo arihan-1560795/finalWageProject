4019a14413e74ac85a7d48a69201f8939c215350
arihan-1560795 <arihan@uw.edu>
2017-05-28 17:47:21 -0700

    Added comments for Arihan's code, made it work

--- server.R
+++ server.R
@@ -1,51 +1,70 @@
 library(dplyr)
 library(plotly)
 library(shiny)
+library(stringr)
 
 setwd('~/GitHub/finalWageProject')
 wage.df <- read.csv('data/annualsalary.csv', stringsAsFactors = FALSE)
 
 shinyServer(function(input, output) { 
-  output$GraphName <- renderPlotly({
-    #process information here
-    return(plot_ly(
-      #Graph data here (data = a, x = b, ...)
-    ))
-  })
+  # output$GraphName <- renderPlotly({
+  #   #process information here
+  #   return(plot_ly(
+  #     #Graph data here (data = a, x = b, ...)
+  #   ))
+  # })
   
-  #Ideas:
-  #Find person, and give all information about them (graph of salary)
-  #median/ average/ diff wage bw depts.
-  #Quartile salary graph
-  #Filter professions by growth/ size; see stats for a particular title
+
+  #findPerson prints a plotLy scatterplot for the given peron's salary for the 4 years of data that 
+  #the government has made available. 
+  #If the person holds multiple positions and earns different salries for each role, the salaries are
+  #summed up for each year and displayed.
+  #The job title's held, alongside by which agency are displayed when hovering over the graph.
+  #Note: assumes that each name is unique and that each person filtered through the given name is the
+  #      same person. Also assumes that the person has worked under the same agency through the range
+  #      for which data is available.
+  #      The given name can have the first and last name in any order, but the function assumes no extra
+  #      information is given (no initials, middle namem, etc.)
   output$findPerson <- renderPlotly({
+    
+    #Split's given name into a vector
     name.vec <- unlist(strsplit(toupper(input$person), split=" "))
+
+    #Filters data based if the given name is contained partially or entirely in the name's column
+    #Assumes no extra initials are given (only first and last name)
     person.df <- wage.df[sapply(strsplit(wage.df$employee_name, split=", "), function(str) all(name.vec %in% str)), ]
-    person.df <- head(person.df, 1)
+    
+    #Converts string salaries into numeric ones
+    person.df$Sal2012 <- as.numeric(gsub(",","",person.df$Sal2012))
+    person.df$Sal2013 <- as.numeric(gsub(",","",person.df$Sal2013))
+    person.df$Sal20141 <- as.numeric(gsub(",","",person.df$Sal20141))
+    person.df$Sal2015 <- as.numeric(gsub(",","",person.df$Sal2015))
+    
+    #Creates a dataframe with only the years and their corresponding salary information
+    #If more than one row was present, the salaries for that given year are summed
+    years <- c('2012', '2013', '2014', '2015')
+    salary <- c(sum(person.df$Sal2012), sum(person.df$Sal2013), sum(person.df$Sal20141), sum(person.df$Sal2015))
+    ys.df <- data.frame(years, salary)
+    
+    #Prepares formatted strings for the person's name, the posts they've held, and the agency they've worked under
+    agency <- str_to_title(person.df$Agency_Title)
+    jobs <- str_to_title(person.df$job_title)
+    name <- str_to_title(input$person)
+    
+    #Returns a scatterplot with the points connected by lines
+    #The x-axis is the years, and y-axis the person's summed salary for that person across all the posts they've held
+    #When hovering a point, the person's name, agency, posts they're holding, and salary for that year become visible
     return(plot_ly(
-      #Graph data here (data = a, x = b, ...)
+      data = ys.df, x = ~years, y = ~salary, type = 'scatter', mode = 'lines', hoverinfo = "text", 
+      text = paste0(' Name: ', name, 
+                    "<br /> Agency:", agency, 
+                    "<br /> Job Title(s): ", paste(jobs, collapse=", "),
+                    "<br /> Salary for ", ys.df$years, ": $", format(ys.df$salary, big.mark=",", scientific=FALSE))
+    )%>% layout(
+      title = paste0(name, "'s salary over time"), 
+      yaxis = list(title = "Salary in USD"), 
+      xaxis = list(title = "Years")
     ))
   })
-  
 })
 
-
-#Following code is scrap; meant to be used for finding stuff about agencies
-# "    person.df <- wage.df %>% filter(grepl(name.vec, employee_name))
-#     group.df <- wage.df %>% group_by(Agency_Title)
-#     group.df <- group.df %>% mutate(test1 = select(as.numeric(gsub(",", "", "23,222")), starts_with("Sal")))
-#     
-#         group.df <- group.df %>% mutate(test1 = select(as.numeric(gsub(",", "", .)), starts_with("Sal")) %>%
-#                rowMeans(na.rm = TRUE))
-#     
-#     group.df <- group.df %>% mutate(average_salary = mean(
-#                                          as.numeric(gsub(",", "", Sal2012)), 
-#                                          as.numeric(gsub(",", "", Sal2013)), 
-#                                          as.numeric(gsub(",", "", Sal20141)), 
-#                                          as.numeric(gsub(",", "", Sal2015))))
-# "    
-# myFun <- function(x) {
-#   c(min = min(x), max = max(x), 
-#     mean = mean(x), median = median(x), 
-#     std = sd(x))
-# }
--- ui.R
+++ ui.R
@@ -2,31 +2,31 @@ library(shiny)
 library(plotly)
 
 shinyUI(navbarPage('FinalProjectName',
-  tabPanel('TabName',
-    titlePanel('TitleBelowTab'),
-    mainPanel(p(paste0("TabDescription")))
-  ),
+  # tabPanel('TabName',
+  #   titlePanel('TitleBelowTab'),
+  #   mainPanel(p(paste0("TabDescription")))
+  # ),
 
-  #Copy the following code as a template---------------
-  tabPanel('GraphDescriptionInTabName',
-  titlePanel('TitleOfGraph'),
-  sidebarLayout(
-    #Sidebar controls go here
-    sidebarPanel(),
-    #Generate graph here (change according to name, type of gaph you want)
-    mainPanel(plotlyOutput("GraphName"))
-  )),
-  #End of copy----------------------------------------                   
+  # #Copy the following code as a template---------------
+  # tabPanel('GraphDescriptionInTabName',
+  # titlePanel('TitleOfGraph'),
+  # sidebarLayout(
+  #   #Sidebar controls go here
+  #   sidebarPanel(),
+  #   #Generate graph here (change according to name, type of gaph you want)
+  #   mainPanel(plotlyOutput("GraphName"))
+  # )),
+  # #End of copy----------------------------------------                   
 
-  #Copy the following code as a template---------------
-  tabPanel('Person',
-   titlePanel('Person1'),
+  #Loads a plotly graph for the given person's salary from 2011 to 2015
+  tabPanel('WA Employee wage',
    sidebarLayout(
      sidebarPanel(
-       textInput("person", label = h3("Text input"), value = "Enter text..."),
-       mainPanel(plotlyOutput("findPerson"))
-   ))
-  #End of copy----------------------------------------                   
-  
+       textInput(inputId = "person", label = h3("See a WA employee's salary!"), value = "Person's first & last name only")
+     ),
+     mainPanel(plotlyOutput("findPerson"))
+   )
+   )
+
     
 ))
